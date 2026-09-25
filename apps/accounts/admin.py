d66b24c959dd76af03c152118d9b3fabb0e7f399
JoachimPuercher <joachim.puercher@gmail.com>
2026-09-25 10:39:43 +0200

    style: apply ruff formatting

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -17,7 +17,15 @@ class UserAdmin(DjangoUserAdmin):
         ("Personal info", {"fields": ("first_name", "last_name", "display_name")}),
         (
             "Permissions",
-            {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")},
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
         ),
         ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
